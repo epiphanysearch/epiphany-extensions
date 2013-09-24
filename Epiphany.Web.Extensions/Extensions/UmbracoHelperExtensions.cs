6a89f34a15d5be511a1b922fc7749b8a68de4f66
Ryan Lewis <ryan.lewis@epiphanysolutions.co.uk>
2013-09-24 15:33:38 +0100

    Initial Epiphany.Web.Extensions commit

---
+++ Epiphany.Web.Extensions/Extensions/UmbracoHelperExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web;
+using uComponents.DataTypes.UrlPicker;
+
+namespace RunForAll.Web.Extensions
+{
+    public static class UmbracoHelperExtensions
+    {
+        /// <summary>
+        /// Extracts the link from a uComponents URL picker
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="pickerData">the URL Picker data from Umbraco</param>
+        /// <param name="defaultUrl">the default thing to link to if the URL picker doesn't have a value set</param>
+        /// <returns>URL as a string</returns>
+        public static string UrlPickerUrl(this UmbracoHelper helper, string pickerData, string defaultUrl)
+        {
+            var url = defaultUrl;
+            var urlPicker = uComponents.DataTypes.UrlPicker.Dto.UrlPickerState.Deserialize(pickerData);
+            if (urlPicker == null) return url;
+            if (!String.IsNullOrEmpty(urlPicker.Url)) return urlPicker.Url;
+            if (urlPicker.NodeId == null || urlPicker.NodeId == 0) return url;
+
+            var nodeId = (int)urlPicker.NodeId;
+            switch (urlPicker.Mode)
+            {
+                case UrlPickerMode.Content:
+                    url = helper.NiceUrl(nodeId);
+                    break;
+                case UrlPickerMode.Media:
+                    var media = helper.TypedMedia(nodeId);
+                    if (media == null || media.GetProperty("umbracoFile") == null) return url;
+                    url = media.GetPropertyValue<string>("umbracoFile");
+                    break;
+            }
+            return url;
+        }
+
+        /// <summary>
+        /// Return an Umbraco pre-value as a IHtmlString
+        /// </summary>
+        /// <param name="helper"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static IHtmlString PreValue(this UmbracoHelper helper, int val)
+        {
+            return new MvcHtmlString(umbraco.library.GetPreValueAsString(val));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-epiphany-extensions
+Epiphany Extensions
 ===================
+
+A collection of utility methods that are common and useful in all Epiphany Umbraco builds.
+
+Requires UmbracoCms.Core 6.1.5 and uComponents 5.5.1 (will probably work in older versions - fork it and mess with the NuGet references)
