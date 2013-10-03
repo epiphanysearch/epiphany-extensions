1ba1b8f96ee3f1254b7eaf5270e24b5592018d52
Tim Payne <tim.payne@epiphanysolutions.co.uk>
2013-10-03 17:04:54 +0100

    Fixes

--- Epiphany.Web.Extensions/Extensions/UmbracoHelperExtensions.cs
+++ Epiphany.Web.Extensions/Extensions/UmbracoHelperExtensions.cs
@@ -4,7 +4,7 @@ using System.Web.Mvc;
 using Umbraco.Web;
 using uComponents.DataTypes.UrlPicker;
 
-namespace RunForAll.Web.Extensions
+namespace Epiphany.Web.Extensions
 {
     public static class UmbracoHelperExtensions
     {
